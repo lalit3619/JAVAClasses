7a6439c01b40f22269383721a90e1f9fa01eacfa
lalit3619 <lakshmikant633@gmail.com>
2022-06-16 19:31:55 +0530

    16.06.2022

---
+++ JavaPrograms/EclipseProgram/Javaclass/src/com/pro1/Student.java
@@ -0,0 +1,23 @@
+package com.pro1;
+
+public class Student {
+String name;
+int age;
+String sub;
+//to intialise non atatic variable
+public Student(String n,int a,String s)
+{
+	name=n;
+	age=a;
+	sub=s;
+	}
+public void display() {
+	System.out.println("Name= "+name+ " Age="+age+" Subject="+sub);
+	
+}
+public static void main(String[] args) {
+
+	Student ms=new Student("Lalit", 62, "Bio");
+	ms.display();
+}
+}
