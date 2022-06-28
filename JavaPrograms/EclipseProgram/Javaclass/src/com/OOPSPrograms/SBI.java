3926115316f5e0d2c04b895b8dfb31fa1c818892
lalit3619 <lakshmikant633@gmail.com>
2022-06-28 23:44:59 +0530

    28.06.2022

--- JavaPrograms/EclipseProgram/Javaclass/src/com/OOPSPrograms/SBI.java
+++ JavaPrograms/EclipseProgram/Javaclass/src/com/OOPSPrograms/SBI.java
@@ -1,8 +1,14 @@
 package com.OOPSPrograms;
 
 public class SBI extends RBI {//inheritance in java //parent child or is a relationship
-	
-	
+	// SIngle Inheritance
+	//Method Overriding in java
+	public void loan_Interest()
+	{
+		System.out.println("Loan intrst= 9.2%");
+		
+	}
+
 	public static void main(String[] args) {
 		SBI s=new SBI();
 		s.loan_Interest();
