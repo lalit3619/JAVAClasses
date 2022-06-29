599bdc3f7e2873da7bee6a314a5d6083f3c95ef0
lalit3619 <lakshmikant633@gmail.com>
2022-06-29 12:17:50 +0530

    Method Hiding

--- JavaPrograms/EclipseProgram/Javaclass/src/com/OOPSPrograms/SBI.java
+++ JavaPrograms/EclipseProgram/Javaclass/src/com/OOPSPrograms/SBI.java
@@ -3,6 +3,11 @@ package com.OOPSPrograms;
 public class SBI extends RBI {//inheritance in java //parent child or is a relationship
 	// SIngle Inheritance
 	//Method Overriding in java
+	//1. Method name should be same
+	//2. Return type should be same
+	//public>protected>default>private
+	// Calling Method
+	// Method Implementation
 	public void loan_Interest()
 	{
 		System.out.println("Loan intrst= 9.2%");
@@ -11,7 +16,9 @@ public class SBI extends RBI {//inheritance in java //parent child or is a relat
 
 	public static void main(String[] args) {
 		SBI s=new SBI();
-		s.loan_Interest();
+		s.loan_Interest();//Called Method
+		//Binding In java
+		//1. Compiler and JRE
 	}
 
 }
